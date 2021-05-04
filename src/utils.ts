6546158b698ea00c468f24803113ee454073cbb0
Henrik Hall <henrik.hall@sonymobile.com>
2021-05-04 13:41:34 +0200

    Initial commit
    Moving types from openapi-ts-backend.

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import {OneOrMany, Params} from 'src/types';
+
+export function findKey<K extends string, V>(obj: Params<K, OneOrMany<V>>, key: string): OneOrMany<V> | undefined {
+  key = key.toLowerCase();
+
+  for (const [k, v] of Object.entries(obj)) {
+    if (k.toLowerCase() === key) {
+      return v as OneOrMany<V>;
+    }
+  }
+}
